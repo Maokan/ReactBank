00027f9d41ac83f09aca0c21294c6d793ab7f8ce
Maokan <hugo.speter@hotmail.com>
2025-11-21 09:04:20 +0100

    Bug Correction

--- react-bank/src/main.jsx
+++ react-bank/src/main.jsx
@@ -6,6 +6,8 @@ import App from './App.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import AccountDetails from './pages/AccountDetails.jsx'
 import Historique from './pages/Historique.jsx'
+import RegisterPage from './Register.jsx'
+import { Register } from './components/APIRequests.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,6 +15,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/account/:id" element={<AccountDetails />} />
         <Route path="/transactions" element={<Historique/>} />
       </Routes>
